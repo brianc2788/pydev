967fd589097589dc4492387d3f1f5637da878e38
userone <brianc2788@gmail.com>
2021-08-02 21:13:21 -0400

    worked on problem 2

---
+++ itknows.py
@@ -0,0 +1,10 @@
+# I found this code somewhere in the dark web.
+# where, exactly, I cannot remember, but...
+# It knows your name.
+funny = True
+while funny:
+    yourname = input('enter your name: ')
+    if yourname == 'your name':
+        funny = False
+    else:
+        print('no, that\'s not it...')
