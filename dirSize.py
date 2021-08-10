f802b2a12828bcba8052e48174b86e10bd2b5261
userone <brianc2788@gmail.com>
2021-08-10 15:38:30 -0400

    Some work done on dirSize.py

--- dirSize.py
+++ dirSize.py
@@ -19,9 +19,12 @@ github.com/user5260/pyscripts
 import os
 
 sizeofcwd = 0
+itemCount = 0
 
 for file in os.listdir(os.getcwd()):
-    print(file + ", " + str(os.path.getsize(os.path.join(os.getcwd(),file))) + ' bytes')
+    print(str(itemCount) + " " + file + ", " + str(os.path.getsize(os.path.join(os.getcwd(),file))) + ' bytes')
     sizeofcwd += os.path.getsize(os.path.join(os.getcwd(),file))
+    print(str(sizeofcwd / 100000)+' MegaBytes\n')
+    itemCount += 1
 
 print('Total Dir Size: ' + str(sizeofcwd) + ' bytes')
