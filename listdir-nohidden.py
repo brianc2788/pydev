62292e456a510a399cdf28aa7299b140a66b5a72
userone <brianc2788@gmail.com>
2021-08-19 20:21:42 -0400

    modified listdir-nohidden.py

--- listdir-nohidden.py
+++ listdir-nohidden.py
@@ -5,6 +5,10 @@ filters out the hidden files/dirs.
 '''
 import os
 
+print('-------------------------------------------------\n'
+     +'listing files/folders in your home directory...\n'
+     +'(excludes hidden files)\n'
+     +'-------------------------------------------------')
 for n in os.listdir('/home/userone'):
     if n[0] != '.':
         print(n)
