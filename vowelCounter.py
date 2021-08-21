2661c7128db67676ab4c6c0f59b72230e65eef88
userone <brianc2788@gmail.com>
2021-08-21 00:48:42 -0400

    added vowelCounter.py

---
+++ vowelCounter.py
@@ -0,0 +1,50 @@
+'''
+vowelCounter.py
+---------------
+User enters a path (absolute) of a plaintext
+file to have the vowels counted and reported
+back by the application.
+'''
+import re
+import os
+
+VowelRegex = re.compile(r'[aeiouAEIOU]')
+txtPath = ' '
+
+#check if dir and file exists.
+#checking for both to be more informative.
+ValidPath = False
+ValidFile = False
+while not ValidPath & ValidFile:
+    txtPath = input('Enter the absolute path of your plaintext file: ')
+
+    if not os.path.isdir(os.path.dirname(txtPath)):
+        print('invalid directory.')
+        ValidPath = False
+    else:
+        ValidPath = True
+        
+        if not os.path.isfile(txtPath):
+            print('file not found.')
+            ValidFile = False
+        else:
+            ValidFile = True
+
+txtFile = open(txtPath)
+# not gonna bother with isopen()
+
+txtContents = txtFile.read()
+txtFile.close()
+
+VowelList = VowelRegex.findall(txtContents)
+
+print('''Vowel Count: -------
+A: '''+str(VowelList.count('a')+VowelList.count('A'))+'''
+E: '''+str(VowelList.count('e')+VowelList.count('E'))+'''
+I: '''+str(VowelList.count('i')+VowelList.count('I'))+'''
+O: '''+str(VowelList.count('o')+VowelList.count('O'))+'''
+U: '''+str(VowelList.count('u')+VowelList.count('U'))+'''
+TOTAL: '''+str(len(VowelList)))
+
+print('''------------------------------
+Thanks for playing.''')
