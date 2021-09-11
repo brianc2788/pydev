39865e7deec330832beabe7743941fa2593487ca
userone <brianc2788@gmail.com>
2021-09-11 19:10:31 -0400

    gmap.py is now functional

--- gmap.py
+++ gmap.py
@@ -1,25 +1,38 @@
 '''
 gmap.py
 -------
-if the user posseses a
-valid address in their
-clipboard, running this
-script will launch
-google maps in a browser;
-centered at, of course,
-the given address.
+prompts the user for an address.
+the regex used to find an address
+is not perfect, but will find most
+addresses in a format akin to:
+'## Street Name, City NY'
+opens google maps in a new browser.
+if the expression doesn't like the
+address you inputted, it won't try
+to open a browser.
 '''
-import os,re,pyperclip
+import re,webbrowser
 
-addrRegex = re.compile(r'''(
-	(\d)+				# street num
-	([a-zA-Z0-9])+		# street name
-	([a-zA-Z]{2,3})		# ave, rd, etc.
-	)''',re.VERBOSE)
+# address for just street addresses with city & state,
+# e.g. '99 Something Drive, Buffalo NY'
+addrRegex = re.compile(r'\d+\s\w+\s\w+\,\s\w+\s[A-Z]{2}')
+# tried verbose format regex; failed.
+# addrRegex = re.compile(r'''(
+# 	([\d]+)			# street num
+# 	(\s) 			# space
+# 	([a-zA-Z0-9]+ 	# street name
+# 	)''',re.VERBOSE)
 
 testStr = input('enter addr: ')
 
-resultsL = addrRegex.findall(testStr)
+resultsList = addrRegex.findall(testStr)
 
-for n in resultsL:
-	print(str(n.groups()))
+if not len(resultsList) > 0:
+	print('couldn\'t detect a valid street address in your input.')
+else:
+	searchStr = 'https://google.com/maps/place/'
+	for n in resultsList:
+		searchStr += n
+		searchStr += '+'
+
+	webbrowser.open_new(searchStr)
