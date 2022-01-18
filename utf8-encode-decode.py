03e080d81c7d56eb78a4a60032cf56ab712ef108
userone <brianc2788@gmail.com>
2022-01-18 12:47:22 -0500

    Changes to utf8-encode-decode script and created, finished mytime1.

--- utf8-encode-decode.py
+++ utf8-encode-decode.py
@@ -6,5 +6,5 @@ to/from unicode.
 string1 = 'hello'
 bytes1 = string1.encode('utf-8')
 print('printing a bytes object with its decode method.')
-print(bytes1.decode('utf-8'))
+print(bytes1.decode('utf-8'),end='')
 
