0639e61fcc1b482392670019c617ce1909dd478d
brianc2788 <brianc2788@gmail.com>
2022-03-31 20:20:21 -0400

    Added 2 new projects.

---
+++ calendar-printer/CalObj.py
@@ -0,0 +1,19 @@
+'''
+CalObj.py
+---------
+Calendar object class.
+Authored by brianc2788@gmail.com
+'''
+
+'''
+CalObj - Calendar Object.
+'''
+class CalObj:
+    def __init__(self,month,year):
+        self.month = month
+        self.year = year
+        self.FirstWeekday = ''
+        self.leapyear = False
+        self.
+    
+    def 
---
+++ calendar-printer/calendar-printer.py
@@ -0,0 +1,9 @@
+'''
+calendar-printer.py
+-------------------
+Description:
+Use CLI to prompt user for month & year. Print calendar (character string).
+
+Authored by brianc2788@gmail.com
+https://github.com/user5260/pyscripts/calendar-printer
+'''
