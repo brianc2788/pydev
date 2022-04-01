83e2a7c2018f8625c25e9008b4b315b25461064b
brianc2788 <brianc2788@gmail.com>
2022-04-01 05:38:20 -0400

    Deleted several files that had little to no purpose or outlived their usefulness. Mostly the former.

--- regexWebsite.py
+++
@@ -1,8 +0,0 @@
-import re
-
-regex1 = re.compile(r'\.\D{3}')
-stringInput = 'something something.net something.com something.org something.eu'
-print(regex1.findall(stringInput))
-
-for match in regex1.findall(stringInput):
-    print(match)
