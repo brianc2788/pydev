d8bae566090ad4531ad7802f8890bde1104a6f6d
brianc2788 <brianc2788@gmail.com>
2022-03-31 21:54:28 -0400

    Some progress & added a commented-out TODO list.

--- brecords-project01/format-br2csv.py
+++ brecords-project01/format-br2csv.py
@@ -17,16 +17,16 @@ test RE with test bottle-record.
 '''
 
 # Regex w/groupings
-BRregex = re.compile(r'''(
-	(\d+\/\d+)				# Date
+BRregex = re.compile(r'''(	# Group 0 - Full match
+	(\d+\/\d+)				# Group 1 - Date
 	\t
-	(\d{1,2}:\d{1,2})		# Time
+	(\d{1,2}:\d{1,2})		# Group 2 - Time
 	\t
-	(\d)					# B. Recieved
+	(\d)					# Group 3 - B. Recieved
 	\t
-	(\d)					# B. Returned
+	(\d)					# Group 4 - B. Returned
 	\t
-	(LEFT|MID|RIGHT)		# Window
+	(LEFT|MID|RIGHT)		# Group 5 - Window
 	\t
 	([\w\s]+)				# Notes. Goes up to newline char.
 	)''',re.VERBOSE)
@@ -37,8 +37,16 @@ ftext = None
 with open(filename,mode='r') as txtfile:
 	ftext = txtfile.read()
 
-matches = BRregex.findall(ftext)
+matchList = BRregex.findall(ftext)
 
-print("matches: ",len(matches))
-for n in matches:
+print("matchList: ",len(matchList))
+for n in matchList:
 	print(n)
+
+# TODO:
+# - create a list or use a module (csv,pandas,etc) to create
+#   an object for writing a new csv file.
+# - use matchObj to get regex groups to assign variables
+#   when writing the new csv file(?).
+
+matchObj = BRregex.search(ftext)
