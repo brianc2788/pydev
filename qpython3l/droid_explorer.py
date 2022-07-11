53ce4b032adf51b96a681a0416062c32d5a9d46a
brianc2788 <brianc2788@gmail.com>
2022-07-10 21:18:32 -0400

    Added dir qpython3l with scripts for qpython android engine.

---
+++ qpython3l/droid_explorer.py
@@ -0,0 +1,37 @@
+#-*-coding:utf8;-*-
+#qpy:console
+"""
+droid_explorer.py
+-----------------
+i hate mobile OSes - lol.
+just going to wrap up a few
+methods from python's os & sys
+modules in some functions for
+*more convenient* means of
+traversing android's filesystem.
+brianc2788@gmail.com
+http://user5260.github.io
+http://github.com/user5260/pydev
+"""
+import os,sys
+
+""" Global I/O prompts. """
+gPromptIn = '[x]'
+gPromptOut_s = '[+]'
+gPromptOut_f = '[-]'
+
+""" If any args, print usage. """
+def parse_args():
+    if len(sys.argv) > 0:
+        print("Usage:\n>>python3 -m droid_explorer.py")
+        sys.exit(0)
+
+""" Main Loop: prompt for commands until quit event. """
+def main():
+    cmd_in = input(sPrompt)
+    #just echo back for now.
+    print(cmd_in)
+
+if __name__ == '__main__':
+    parse_args()
+    main()
