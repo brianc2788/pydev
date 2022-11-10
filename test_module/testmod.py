590f8a612a2c922981dd45f8274fa5860767f574
brianc2788 <brianc2788@gmail.com>
2022-11-10 14:20:24 -0500

    Added description to source code for test_module.

--- test_module/testmod.py
+++ test_module/testmod.py
@@ -1,12 +1,16 @@
 '''
-Following instructions for how to build a module.
-From 'Mastering Python for Networking and Security',
-by Jose Manuel Ortega
-
-[Module Description]
-A test module, which will just print something.
-
-brianc2788@gmail.com
+testmod.py
+--------------------
+Bare-bones module for Python3; with notes.
+--------------------
+Must be located in Python's import path or
+this module must be located in the interpreter's
+current working directory.
+Import module name, sans .py extension, can use
+'from' statements, if necessary, to direct the
+interpreter to the module's location.
+--------------------
+http://brianc2788.github.io/
 '''
 
 def status():
--- test_module/tmmain.py
+++ test_module/tmmain.py
@@ -1,3 +1,18 @@
+'''
+tmmain.py
+-------------------------
+Python3 - Test Module
+-------------------------
+This script will only work if testmod.py is located
+in python's import path or the interpreter is run in
+the same directory.
+If the interpreter run in the root directory, the
+statement `from test_module import testmod` would be
+required; Or, again, the test_module dir could be loc-
+ated in python's import path.
+-------------------------
+http://brianc2788.github.io/
+'''
 import testmod
 def main():
     testmod.status()
